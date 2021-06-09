e16f2355e26abe27d673c92b82e1146ce5f5444f
Caio Felipe <imcaiofelipe@gmail.com>
2021-06-08 21:29:43 -0300

    CRUD implementado.

--- src/main/java/market_project/SistemaController.java
+++ src/main/java/market_project/SistemaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import market_project.repositories.ProductsDAO;
 
 
-
 @Controller
 public class SistemaController {
 	@Autowired
@@ -47,35 +46,23 @@ public class SistemaController {
 		return "produtos";
 	}
 	
-	@GetMapping("/cadastrar")
-	public String produtosForm(Model model, @RequestParam String page, @RequestParam(name = "id") Integer contentId) {
-		/* class ResponsePageConfig {
-			String category;
-			String method;
-			
-			public ResponsePageConfig(String category, String method) {
-				this.category = category;
-				this.method = method;
-			}
-		}
-		
-		ResponsePageConfig config = new ResponsePageConfig(page, "cadastro");
-		
-		model.addAttribute("config", config); */
-		
-		model.addAttribute("category", page);
-		model.addAttribute("method", "create");
-		
-		System.out.println(contentId);
-		
-		switch(page) {
+	@GetMapping("/form")
+	public String produtosForm(Model model, 
+			@RequestParam String category, 
+			@RequestParam(name = "id", required = false) Integer contentId,
+			@RequestParam(required = false) String method) {
+		model.addAttribute("category", category);
+		model.addAttribute("method", method);
+	
+		switch(category) {
 			case "produtos":
-				if(contentId != null) {
+				if(method.equals("update") && contentId != null) {
 					Products product = this.products.getOne(contentId);
 					
 					model.addAttribute("product", product);
 				} else {
-					model.addAttribute("product", new Products());
+					Products teste = new Products();
+					model.addAttribute("product", teste);
 				}
 				
 				return "produtos-form";
@@ -84,27 +71,45 @@ public class SistemaController {
 		}
 	}
 	
-	@PostMapping("/cadastrar/{category}")
+	@PostMapping("/create/{category}")
 	public String create(@PathVariable("category") String category, Products product) {
-		product.printOnConsole();
-		
 		switch(category) {
 			case "produtos":
 				this.products.save(product);
 				break;
 			default:
-				return "redirect:/" + category;
+				// ...
 		}
 	
 		return "redirect:/" + category;
 	}
 	
 	
-	/* @GetMapping("/editar")
-	public String updatePage(@PathVariable("category") String category, Products product) {
-		product.printOnConsole();
+	@PostMapping("/update/{category}")
+	public String update(@PathVariable("category") String category, Products product) {		
+		switch(category) {
+			case "produtos":
+				this.products.save(product);
+				break;
+			default:
+				// ...
+		}
 		
+		return "redirect:/" + category;
+	}
+	
+	@GetMapping("/delete/{category}")
+	public String delete(@PathVariable("category") String category, 
+			@RequestParam(name = "id") Integer contentId) {
+		switch(category) {
+			case "produtos":
+				this.products.deleteById(contentId);
+				break;
+			default:
+				// ...
+		}
 		
-	} */
+		return "redirect:/" + category;
+	}
 	
 }
